341e3cdfe551dcd9c5b61844be0b750422b060d1
raha <elahe.dstn@gmail.com>
2020-07-13 14:53:27 +0430

    add go mod

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	//import the redigo/redis package
+	"github.com/gomodule/redigo/redis"
+)
 func main() {
-	
+	//Establish a connection to yhe redis server listening on the default port 6379
+	conn, err := redis.
 }
