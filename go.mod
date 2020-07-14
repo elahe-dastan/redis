c676b46c7c0e3da47e71ed2b4e1540ec761f80e0
raha <elahe.dstn@gmail.com>
2020-07-14 13:10:20 +0430

    Added a hash to redis

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module redis
 
 go 1.13
+
+require github.com/gomodule/redigo/redis v0.0.0-20200429221454-e14091dffc1b
--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	//import the redigo/redis package
+
+	"log"
+
 	"github.com/gomodule/redigo/redis"
 )
 func main() {
 	//Establish a connection to yhe redis server listening on the default port 6379
-	conn, err := redis.
+	conn, err := redis.Dial("tcp", "localhost:6379")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = conn.Do("HMSET", "album2", "title", "Electric ladyland", "artist", "Jimi Hendrix", "price", 4.95, "likes", 8)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
 }
